4cc3b044382df486616fa78af0c224fc230bfeac
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2020-12-26 12:17:49 +0900

    fix build error

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -27,13 +27,14 @@ import android.app.*;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.drawable.Icon;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
-import org.json.JSONObject;
+
 import androidx.core.app.NotificationCompat;
+
+import org.json.JSONObject;
 
 import static android.os.PowerManager.PARTIAL_WAKE_LOCK;
 
